ce83a8c0e7ebeae6ba8d4fb6c12092e5fa480e2f
georgetegral <georgetegral@gmail.com>
2020-04-11 20:02:14 -0500

    finished cron script

--- cron.php
+++ cron.php
@@ -18,6 +18,13 @@ function getLon($ip){
     return $obj->lon;
 }
 
+function changeLocale($ip){
+    $ipInfo = file_get_contents('http://ip-api.com/json/' . $ip);
+    $ipInfo = json_decode($ipInfo);
+    $timezone = $ipInfo->timezone;
+    date_default_timezone_set($timezone);
+}
+
 function getWeatherId($lat,$lon){
     $apiKey= '7fcc45092b8560eb0f356add9bd91dad';
     $url = 'https://api.openweathermap.org/data/2.5/weather?lat='.$lat.'&lon='.$lon.'&APPID='.$apiKey;
@@ -26,17 +33,57 @@ function getWeatherId($lat,$lon){
     $res = $res["weather"];
     $res = $res[0];
     $res = $res["id"];
+    $res = (string)$res;
     return $res;
 }
 
+function water($pin){
+    exec("sudo gpio mode ".$pin." in");
+    sleep(7);
+    exec("sudo gpio mode ".$pin." out");
+}
+
+function saveToDB($watered){
+    $servername = "localhost";
+    $username = "admin";
+    $password = "pajarito";
+    $database = "logPiWater";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        $timestamp = new DateTime();
+        $timestamp = $timestamp->getTimestamp();
+        $timestamp = date('Y-m-d H:i:s',$timestamp);
+        $sql="INSERT INTO logs (time, watered) VALUES ('$timestamp', '$watered')";
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+
+//Cambiar el número para cambiar el pin de GPIO
 define("pin",2);
 
 $ip = getIp();
+changeLocale($ip);
 $lat = getLat($ip);
 $lon = getLon($ip);
-$weatherId = getWeatherId($lat,$lon);
-echo $weatherId;
-
-
+$id = getWeatherId($lat,$lon);
+$watered = 0;
+if( $id[0] == "2" || $id[0] == "3" || $id[0] == "5" || $id[0] == "6"){
+    if( $id== "210" || $id == "211" || $id == "212" || $id == "221"){
+        water(pin);
+        $watered = 1;
+    }
+} else {
+    water(pin);
+    $watered = 1;
+}
+saveToDB($watered);
 
 ?>
